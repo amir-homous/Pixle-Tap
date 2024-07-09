9b26b79c9968e8a73d536286e2d6710ba88dcb7a
Amir hossein Mousavi <amir.homos@gmail.com>
2024-07-09 17:14:16 +0330

    Update pixeltap-autoclicker.user.js

--- pixeltap-autoclicker.user.js
+++ pixeltap-autoclicker.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @grant        none
 // @version      1.3
-// @author       mudachyo
+// @author       amir-homous
 // @name         PixelTap Autoclicker
 // @description  21.06.2024, 21:31:29
 // @match        *://sexyzbot.pxlvrs.io/*
 // @homepage     https://github.com/mudachyo/PixelTap
 // @icon         https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ8fIh36hOYyMEv7XiDsX0EMOP2MC18Trptg&s
-// @downloadURL  https://github.com/mudachyo/PixelTap/raw/main/pixeltap-autoclicker.user.js
-// @updateURL    https://github.com/mudachyo/PixelTap/raw/main/pixeltap-autoclicker.user.js
+// @downloadURL  https://github.com/amir-homous/PixelTap/raw/main/pixeltap-autoclicker.user.js
+// @updateURL    https://github.com/amir-homous/PixelTap/raw/main/pixeltap-autoclicker.user.js
 // ==/UserScript==
 
 const styles = {
